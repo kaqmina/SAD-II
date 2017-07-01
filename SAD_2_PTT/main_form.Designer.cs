270dc258c5314bd59040228aa3c89cb4810a6c02
IDK <mmorpg.momo@gmail.com>
2017-07-01 15:51:14 +0800

    Check font..

--- SAD_2_PTT/main_form.Designer.cs
+++ SAD_2_PTT/main_form.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.controlbox = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.btn_min = new System.Windows.Forms.Button();
             this.btn_max = new System.Windows.Forms.Button();
             this.btn_close = new System.Windows.Forms.Button();
@@ -46,14 +48,20 @@
             this.btn_pwd = new System.Windows.Forms.Button();
             this.btn_dashboard = new System.Windows.Forms.Button();
             this.btn_profilepic = new System.Windows.Forms.Button();
+            this.slide_in = new System.Windows.Forms.Timer(this.components);
+            this.slide_out = new System.Windows.Forms.Timer(this.components);
+            this.main_content = new System.Windows.Forms.Panel();
+            this.heading1 = new System.Windows.Forms.Label();
             this.controlbox.SuspendLayout();
             this.main_tab.SuspendLayout();
             this.side_tab.SuspendLayout();
+            this.main_content.SuspendLayout();
             this.SuspendLayout();
             // 
             // controlbox
             // 
             this.controlbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.controlbox.Controls.Add(this.button1);
             this.controlbox.Controls.Add(this.btn_min);
             this.controlbox.Controls.Add(this.btn_max);
             this.controlbox.Controls.Add(this.btn_close);
@@ -64,6 +72,22 @@
             this.controlbox.Name = "controlbox";
             this.controlbox.Size = new System.Drawing.Size(1102, 31);
             this.controlbox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(966, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "N";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // btn_min
             // 
@@ -222,7 +246,7 @@
             this.button7.ForeColor = System.Drawing.Color.White;
             this.button7.Location = new System.Drawing.Point(0, 438);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(71, 73);
+            this.button7.Size = new System.Drawing.Size(71, 55);
             this.button7.TabIndex = 10;
             this.button7.Text = "-";
             this.button7.UseVisualStyleBackColor = false;
@@ -334,11 +358,40 @@
             this.btn_profilepic.UseVisualStyleBackColor = false;
             this.btn_profilepic.Click += new System.EventHandler(this.btn_profilepic_Click);
             // 
+            // slide_in
+            // 
+            this.slide_in.Tick += new System.EventHandler(this.slide_in_Tick);
+            // 
+            // slide_out
+            // 
+            this.slide_out.Interval = 40;
+            this.slide_out.Tick += new System.EventHandler(this.slide_out_Tick);
+            // 
+            // main_content
+            // 
+            this.main_content.Controls.Add(this.heading1);
+            this.main_content.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.main_content.Font = new System.Drawing.Font("Trench", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.main_content.Location = new System.Drawing.Point(283, 31);
+            this.main_content.Name = "main_content";
+            this.main_content.Size = new System.Drawing.Size(819, 608);
+            this.main_content.TabIndex = 3;
+            // 
+            // heading1
+            // 
+            this.heading1.AutoSize = true;
+            this.heading1.Location = new System.Drawing.Point(20, 19);
+            this.heading1.Name = "heading1";
+            this.heading1.Size = new System.Drawing.Size(156, 29);
+            this.heading1.TabIndex = 0;
+            this.heading1.Text = "Dashboard";
+            // 
             // main_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1102, 639);
+            this.Controls.Add(this.main_content);
             this.Controls.Add(this.main_tab);
             this.Controls.Add(this.controlbox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -349,6 +402,8 @@
             this.controlbox.PerformLayout();
             this.main_tab.ResumeLayout(false);
             this.side_tab.ResumeLayout(false);
+            this.main_content.ResumeLayout(false);
+            this.main_content.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -373,6 +428,11 @@
         public System.Windows.Forms.Button btn_pwd;
         public System.Windows.Forms.Button btn_dashboard;
         public System.Windows.Forms.Button btn_profilepic;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer slide_in;
+        private System.Windows.Forms.Timer slide_out;
+        private System.Windows.Forms.Panel main_content;
+        private System.Windows.Forms.Label heading1;
     }
 }
 
--- SAD_2_PTT/main_form.cs
+++ SAD_2_PTT/main_form.cs
@@ -43,42 +43,49 @@ namespace SAD_2_PTT
         {
             main_btn.btn_method(btn_current, btn_profilepic);
             btn_current = btn_profilepic;
+            //slide_out.Start();
         }
 
         private void btn_dashboard_Click(object sender, EventArgs e)
         {
             main_btn.btn_method(btn_current, btn_dashboard);
             btn_current = btn_dashboard;
+            //slide_in.Start();
         }
 
         private void btn_pwd_Click(object sender, EventArgs e)
         {
             main_btn.btn_method(btn_current, btn_pwd);
             btn_current = btn_pwd;
+            //slide_out.Start();
         }
 
         private void btn_device_Click(object sender, EventArgs e)
         {
             main_btn.btn_method(btn_current, btn_device);
             btn_current = btn_device;
+            //slide_out.Start();
         }
 
         private void btn_projects_Click(object sender, EventArgs e)
         {
             main_btn.btn_method(btn_current, btn_projects);
             btn_current = btn_projects;
+            //slide_out.Start();
         }
 
         private void btn_reports_Click(object sender, EventArgs e)
         {
             main_btn.btn_method(btn_current, btn_reports);
             btn_current = btn_reports;
+            //slide_out.Start();
         }
 
         private void btn_settings_Click(object sender, EventArgs e)
         {
             main_btn.btn_method(btn_current, btn_settings);
             btn_current = btn_settings;
+            //slide_out.Start();
         }
         #endregion
 
@@ -86,9 +93,40 @@ namespace SAD_2_PTT
         private void main_form_Load(object sender, EventArgs e)
         {
             btn_current = btn_dashboard;
+            btn_dashboard_Click(sender, e);
+            main_properties();
+        }
+
+        private void main_properties()
+        {
+            sidenav.Size = new Size(212, 608);
+            main_tab.Size = new Size(283, 608); //283, 608 <-- main_tab Size
+            side_tab.Size = new Size(71, 608);
         }
         #endregion
 
+        #region Timers - Slide_in and Slide_out TM - 00
+
+        private void slide_out_Tick(object sender, EventArgs e)
+        {
+            while (main_tab.Width != 283)
+            {
+                main_tab.Width++;
+            }
+
+            slide_out.Stop();
+        }
+
+        private void slide_in_Tick(object sender, EventArgs e)
+        {
+            while (main_tab.Width != 71)
+            {
+                main_tab.Width--;
+            }
+
+            slide_in.Stop();
+        }
+        #endregion
 
     }
 }
