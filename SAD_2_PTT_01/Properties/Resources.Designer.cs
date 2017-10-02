ab61a2ece1e4254318ec43c181c43507e4c0dbb0
cpglarena <pia.ess13@gmail.com>
2017-10-02 18:11:31 +0800

    [pdao_logo] aw

--- SAD_2_PTT_01/Properties/Resources.Designer.cs
+++ SAD_2_PTT_01/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SAD_2_PTT_01.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -326,6 +326,16 @@ namespace SAD_2_PTT_01.Properties {
         internal static System.Drawing.Bitmap num9 {
             get {
                 object obj = ResourceManager.GetObject("num9", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pdao_logo {
+            get {
+                object obj = ResourceManager.GetObject("pdao_logo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
